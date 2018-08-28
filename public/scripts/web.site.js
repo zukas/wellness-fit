058a87ccc9e5b1cc3c9c2b3fc72f4e36d0c2737b
zukas <julius.zukauskas@gmx.com>
2018-08-28 13:58:11 +0200

    Update to release version

--- public/scripts/web.site.js
+++ public/scripts/web.site.js
@@ -38,4 +38,38 @@
     	}
     }());
 
+    root.init_maps = function () {
+		var map_obj     = new google.maps.Map(document.getElementById("map"), {
+                center: new google.maps.LatLng(52.152296, 21.035628),
+                zoom : 17,
+                mapTypeControl : false,
+                fullscreenControl: false
+            }),
+            marker      = new google.maps.Marker({ map : map_obj }),
+            service     = new google.maps.places.PlacesService(map_obj),
+            info        = document.createElement("div"),
+            name        = document.createElement("div"),
+            address     = document.createElement("div");
+
+        info.className = "location-info";
+        name.className = "location-info-name";
+        address.className = "location-info-address";
+
+        info.appendChild(name);
+        info.appendChild(address);
+
+        map_obj.controls[google.maps.ControlPosition.TOP_LEFT].push(info);
+
+
+        service.getDetails({placeId : "ChIJP2R9Lq4zGUcREPtrr3nK1t0" }, function (place, status) {
+            marker.setPlace({
+                placeId: place.place_id,
+                location: place.geometry.location
+            });
+
+            name.innerHTML = place.name;
+            address.innerHTML = place.formatted_address;
+        });
+    }
+
 }(this));
